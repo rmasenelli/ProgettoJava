d1c3d74accb3572ec7b2bbe584aab086334b953b
Riccardo Masenelli <riccardo.masenelli@gmail.com>
2020-05-02 16:39:50 +0200

    Correzzione LoyaltyCard

--- Classi/LoyaltyCard.java
+++ Classi/LoyaltyCard.java
@@ -3,14 +3,16 @@ import java.time.LocalDate;
 
 public class LoyaltyCard {
 
-    private static final int code = 0;
+    private static int id = 0;
+    private final int code;
     private final LocalDate emissionDate;
     private int points;
 
 
     public LoyaltyCard(){
-        LoyaltyCard.code++;
-        LoyaltyCard.emissionDate = LocalDate.now();
+        LoyaltyCard.id++;
+        this.code = id;
+        this.emissionDate = LocalDate.now();
         this.points = 0;
     }
 
