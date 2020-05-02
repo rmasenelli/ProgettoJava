9aaa604e48e24503be83d6bc10334cc747451513
Riccardo Masenelli <riccardo.masenelli@gmail.com>
2020-05-02 16:04:15 +0200

    Aggiunta ShoppingManager

--- Classi/ShoppingManager.java
+++ Classi/ShoppingManager.java
@@ -1,10 +1,42 @@
 
 public class ShoppingManager {
 
-    private final static int serialNumber;
-    private final static Strinh name;
-    private final static String surname;
-    private final static String role;
-    private final static String login;
-    private final static String password
+    private static int serialNumber = 0;
+    private final String name;
+    private final String surname;
+    private String role;
+    private final String login;
+    private String password;
+
+    public ShoppingManager(String name, String surname, String role, String login, String password){
+
+        ShoppingManager.serialNumber++;
+        this.name = name;
+        this.surname = surname;
+        this.role = role;
+        this.login = login;
+        this.password = password;
+    }
+
+    public String getName(){
+
+        return this.name;
+    }
+
+    public String getSurname(){
+
+        return this.surname;
+    }
+
+    public String getRole(){
+
+        return this.role;
+    }
+
+    public String getLogin(){
+
+        return this.login;
+    }
+
+    //getPassword
 }
