3cf479b723115c6c8f009ae0478b48ab36ec2ece
Riccardo Masenelli <riccardo.masenelli@gmail.com>
2020-05-02 13:06:13 +0200

    Aggiunte a LoyaltyCard

--- LoyaltyCard.java
+++ LoyaltyCard.java
@@ -1,5 +1,17 @@
 
+import java.time.LocalDate;
 
 public class LoyaltyCard {
+
+    private static final int code;
+    private static final LocalDate emissionDate;
+    private static int points;
+
+
+    public LoyaltyCard(){
+        LoyaltyCard.code++;
+        LoyaltyCard.emissionDate = LocalDate.now();
+        this.points = 0;
+    }
 
 }
