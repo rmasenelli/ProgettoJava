df338d29fb768b7fd1cc0c06512d3b91f4e57767
Riccardo Masenelli <riccardo.masenelli@gmail.com>
2020-05-02 12:39:35 +0200

    Costruttore Product

--- Product.java
+++ Product.java
@@ -1,5 +1,25 @@
-
+import java.awt.*;
 
 public class Product {
 
+    private static int code = 0;
+    private final String name;
+    private final String brand;
+    private int quantity; // non final puo cambiare
+    private final double price;
+    private final Image image; 
+
+    public Product(String name, String brand, int quantity, double price){
+
+        code++;
+        this.name = name;
+        this.brand = brand;
+        this.quantity = quantity;
+        this.price = price;
+        this.image = setImage(); // dobbiamo fare che in base al nome poi si apre il file giusto automaticamente 
+    }
+
+    private Image setImage(){
+        return null;
+    }
 }
